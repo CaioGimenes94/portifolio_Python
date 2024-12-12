c76f32333cdf5759a8d7a240315da8e6f90fb2cc
Kagimal <gimenescaio13@gmail.com>
2024-12-12 13:43:44 -0300

    Adiciona calculadora básica em Python com validação de operador e prevenção de divisão por zero.

---
+++ calculadora_simples/calcPy.py
@@ -0,0 +1,19 @@
+x = float(input('Insira um número: '))
+param = ''
+l1 = ['+', '-', '/', '*']
+
+while param not in l1:
+    param = input('Digite um operador válido (+ - / *): ')
+
+y = float(input('Insira outro número: '))
+
+if param == '+':
+      print(f'O resultado é {x + y}')
+elif param == '-':
+    print(f'O resultado é {x - y}')
+elif param == '/':
+    while y == 0:
+        y = float(input('Não é possível dividir por zero. Insira outro valor: '))
+    print(f'O resultado é {x / y}')
+elif param == '*':
+    print(f'O resultado é {x * y}')
