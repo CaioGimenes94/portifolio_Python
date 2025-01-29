b4fb0311bbfb52a109960660c7aecb4b5ae3c03a
CaioGimenes94 <gimenescaio@hotmail.com>
2025-01-29 15:13:54 -0300

    Melhorias na lógica principal do main.py

--- projeto_bitntime/main.py
+++ projeto_bitntime/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from app.api_handler import checar_btc
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    preco_btc = checar_btc()  # Chama a função para obter o preço
+    if preco_btc:
+        return render_template('index.html', preco_btc=preco_btc)
+    else:
+        return "Erro ao obter o preço do Bitcoin", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
